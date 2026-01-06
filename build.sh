6d7f0f58891c60350acd5086fb3ed5f2214f726e
cedelis <cedelis@uillinois.edu>
2026-01-06 12:39:32 -0500

    RT 139221

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-VER=0.8
+VER=0.9
 #docker build . -t cedelis/alma-inventory-node:${VER} --progress=plain --no-cache
 docker build . -t cedelis/alma-inventory-node:${VER} # --progress=plain --no-cache
